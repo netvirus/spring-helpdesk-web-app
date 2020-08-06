b9ceb13b610576cd9c8fa7c6f344184a3aedfda9
netvirus <yariknetvirus@gmail.com>
2020-08-06 12:58:51 +0700

    Added sql files

---
+++ src/main/resources/managers.sql
@@ -0,0 +1,7 @@
+create table managers
+(
+    id       int auto_increment
+        primary key,
+    fullname varchar(60) not null
+);
+
---
+++ src/main/resources/messages.sql
@@ -0,0 +1,7 @@
+create table messages
+(
+    id      int auto_increment
+        primary key,
+    message varchar(255) default '' not null
+);
+
